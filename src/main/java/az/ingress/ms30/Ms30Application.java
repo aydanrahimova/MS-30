15a10dcf4c4640c905f4ed94f35dad3cd0714448
AydanRahimova <aydanragimova28@gmail.com>
2025-05-20 21:32:00 +0400

    initial commit

---
+++ src/main/java/az/ingress/ms30/Ms30Application.java
@@ -0,0 +1,13 @@
+package az.ingress.ms30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms30Application.class, args);
+    }
+
+}
---
+++ src/test/java/az/ingress/ms30/Ms30ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.ms30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms30ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
